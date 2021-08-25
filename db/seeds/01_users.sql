9c09b573d231fe5173bac47727059207a75671e7
chandni <chandnivaghasia11@gmail.com>
2021-08-25 17:54:17 -0400

    Fix some design tags

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -10,13 +10,13 @@ VALUES ('Aphason', 'Brillums', 'ap@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4
 ('Mitchell', 'Zelpin', 'Mit@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 4565778765, '12 Saint Elijah Boulevard', 'Victoria', 'Canada', 'WGH1GH', false);
 
 INSERT INTO cars (photo, car_name, year_created, colour, brand, car_description, price, isSold, owner_id)
-VALUES ('/images/image.jpeg','honda civic', 2002, 'black', 'Honda', 'slowest car around', 5000, false, NULL),
-('/images/image.jpeg','lamborghini turbo v3', 2019, 'blue', 'lamborghini', 'sonic fast with added turbo extensions', 1200000, false, NULL),
-('/images/image.jpeg','maserati fusion', 2015, 'teel', 'maserati', 'perfect car for a friday night date', 200000, false, NULL),
-('/images/image.jpeg','suburban truck', 2021, 'army camo', 'SUV', '2 ft off the ground solid rock traversing vechile', 180000, false, NULL),
-('/images/image.jpeg','trailer-g2', 2008, 'white', 'Timmy Trailers', 'good for a camping trip, no refunds included', 5500, false, NULL),
-('/images/image.jpeg','speed demon 3000 to boat convertible', 2025, 'steel-white', 'f1-racing', 'a car never to thought exist before', 15000050, false, NULL),
-('/images/image.jpeg','4 wheeler delta', 2004, 'crimson', 'Duggys Durby', 'An old dog with new tricks', 25000, false, NULL),
+VALUES ('/images/1.jpg','honda civic', 2002, 'black', 'Honda', 'slowest car around', 5000, false, NULL),
+('/images/2.jpg','lamborghini turbo v3', 2019, 'blue', 'lamborghini', 'sonic fast with added turbo extensions', 1200000, false, NULL),
+('/images/3.jpg','maserati fusion', 2015, 'teel', 'maserati', 'perfect car for a friday night date', 200000, false, NULL),
+('/images/4.jpeg','suburban truck', 2021, 'army camo', 'SUV', '2 ft off the ground solid rock traversing vechile', 180000, false, NULL),
+('/images/5.jpg','trailer-g2', 2008, 'white', 'Timmy Trailers', 'good for a camping trip, no refunds included', 5500, false, NULL),
+('/images/6.jpg','speed demon 3000 to boat convertible', 2025, 'steel-white', 'f1-racing', 'a car never to thought exist before', 15000050, false, NULL),
+('/images/7.jpg','4 wheeler delta', 2004, 'crimson', 'Duggys Durby', 'An old dog with new tricks', 25000, false, NULL),
 ('/images/image.jpeg','tricycle', 2012, 'lucky-red', 'trikes for kids', 'Good for kids ages 3-6', 40, false, NULL),
 ('/images/image.jpeg','1998 Bently', 1998, 'grey', 'Bently', 'A good old relic of the past', 7600, false, NULL);
 
